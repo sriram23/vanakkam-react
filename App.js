5538cd64eb79db7ae89853849d58e6c990704a7b
Sriram <sriramsubramanian23@gmail.com>
2023-10-14 14:31:31 +0530

    [Ex-10] - Even more complex, 2 children for parent with their own children

--- App.js
+++ App.js
@@ -5,12 +5,19 @@ const heading = React.createElement("h1", {id: "heading"}, "Hello World From Rea
 console.log(heading)
 
 // Creating a complex html structure
-const parent = React.createElement("div", {id: "parent"}, 
+const parent = React.createElement("div", {id: "parent"},
+                                [
 								  React.createElement("div", {id: "child"},
 								  [
                                     React.createElement("h1", {}, "I'm a h1 tag"),
                                     React.createElement("h2", {}, "I'm a h2 tag")
-                                  ]))
+                                  ]),
+                                  React.createElement("div", {id: "child2"},
+								  [
+                                    React.createElement("h1", {}, "I'm a h1 tag"),
+                                    React.createElement("h2", {}, "I'm a h2 tag")
+                                  ])
+                                ])
 // Consoling the parent to see what's there.
 console.log(parent)
 
