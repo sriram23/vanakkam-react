f7203ed93a7f86163d539c963e8e7a051891a59e
Sriram <sriramsubramanian23@gmail.com>
2023-10-14 13:35:27 +0530

    [Ex-6] - Added attributes to the newly created heading

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 // Creating a React element
-const heading = React.createElement("h1", {}, "Hello World From React!")
+const heading = React.createElement("h1", {id: "heading"}, "Hello World From React!")
 // Creating the DOM ROOT element
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // Rendering the created element in the DOM
