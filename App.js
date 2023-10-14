b7c1dfeae5875a278bf7cbb0decbd9e65a125dcf
Sriram <sriramsubramanian23@gmail.com>
2023-10-14 14:21:19 +0530

    [Ex-9] - Created a sibling to h1 element (Nothing but even more complex structure)

--- App.js
+++ App.js
@@ -3,7 +3,18 @@ const heading = React.createElement("h1", {id: "heading"}, "Hello World From Rea
 
 // Consoling the heading to see what's there.
 console.log(heading)
+
+// Creating a complex html structure
+const parent = React.createElement("div", {id: "parent"}, 
+								  React.createElement("div", {id: "child"},
+								  [
+                                    React.createElement("h1", {}, "I'm a h1 tag"),
+                                    React.createElement("h2", {}, "I'm a h2 tag")
+                                  ]))
+// Consoling the parent to see what's there.
+console.log(parent)
+
 // Creating the DOM ROOT element
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // Rendering the created element in the DOM
-root.render(heading);
+root.render(parent);
