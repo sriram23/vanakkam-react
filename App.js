f81c2846cf554a1a25ad30f2885363490314cb27
Sriram <sriramsubramanian23@gmail.com>
2023-10-14 13:57:42 +0530

    [Ex-8] - Consoling heading to see the React Element h1

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 // Creating a React element
 const heading = React.createElement("h1", {id: "heading"}, "Hello World From React!")
+
+// Consoling the heading to see what's there.
+console.log(heading)
 // Creating the DOM ROOT element
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // Rendering the created element in the DOM
