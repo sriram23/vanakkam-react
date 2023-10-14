70e60c66d1c3fea5425108a55fc34f5a9454d225
Sriram <sriramsubramanian23@gmail.com>
2023-10-14 13:24:01 +0530

    [Ex-5] - Moved the js code to a separate file

---
+++ App.js
@@ -0,0 +1,6 @@
+// Creating a React element
+const heading = React.createElement("h1", {}, "Hello World From React!")
+// Creating the DOM ROOT element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// Rendering the created element in the DOM
+root.render(heading);
