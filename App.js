5ec96cf04c32a0c992d650ab107b1f1a9a7cda40
Sriram <sriramsubramanian23@gmail.com>
2023-10-24 17:43:33 +0530

    [Ex-18] - Recap of the react render in App.js

--- App.js
+++ App.js
@@ -1,29 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// Creating a React element
-const heading = React.createElement("h1", {id: "heading"}, "Hello World From React!")
 
-// Consoling the heading to see what's there.
+const heading = React.createElement('h1', {id:'heading'}, "Vanakkam React 🚀")
 console.log(heading)
-
-// Creating a complex html structure
-const parent = React.createElement("div", {id: "parent"},
-                                [
-								  React.createElement("div", {id: "child"},
-								  [
-                                    React.createElement("h1", {}, "I'm a h1 tag"),
-                                    React.createElement("h2", {}, "I'm a h2 tag")
-                                  ]),
-                                  React.createElement("div", {id: "child2"},
-								  [
-                                    React.createElement("h1", {}, "I'm a h1 tag"),
-                                    React.createElement("h2", {}, "I'm a h2 tag")
-                                  ])
-                                ])
-// Consoling the parent to see what's there.
-console.log(parent)
-
-// Creating the DOM ROOT element
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// Rendering the created element in the DOM
-root.render(parent);
+const root  = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
